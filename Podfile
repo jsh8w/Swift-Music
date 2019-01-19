5bafaf4f8843bed263694dcbb636a8ceb978a3f2
James Shaw <jamesshaw95@googlemail.com>
2019-01-19 16:39:57 +0000

    Initials Xcode, CocoaPods and project setup.

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'Swift-Weather' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Sourcery'
+  pod 'ViewAnimator'
+
+  target 'Swift-WeatherTests' do
+    inherit! :search_paths
+  end
+end
---
+++ Swift-Weather/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-Weather
+//
+//  Created by James Shaw on 19/01/2019.
+//  Copyright © 2019 James Shaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
